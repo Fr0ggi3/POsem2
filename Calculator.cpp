7c574146307e1a69649f8a427e51452fa2395f54
Fr0ggi3 <287084@student.pwr.edu.pl>
2025-03-22 15:49:08 +0100

    Almost complete console based calc
    Range checking of opt is not working properly, recognises if number is from range,  recognises if input is not a number, but has a problem in recognising "1-" as an error.

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,14 +1,11 @@
 #include <iostream>
+#include <limits>
 using namespace std;
 double mem;
 bool mem_used; //sprawdza czy uzyto pamieci, jesli tak to pobieramy tylko jedna liczbe, jesli nie to 2
-
-void warning(int const a) {
-    switch (a) {
-        case 1:
-        cout<<"Dzielenie przez 0 jest niedozwolone \n"<<endl;
-        break;
-    }
+//dodatkowo przeliczanie baz -> podac baze, liczbe i na jaki system liczbowy go zmienić
+void warning() {
+        cout<<"Dzielenie przez 0 jest niedozwolone, wyswietle poprzednia wartosc \n"<<endl;
 }
 double add(const double a, const double b) {
     return a + b;
@@ -22,46 +19,90 @@ double mult(const double a, const double b) {
 }
 double div(const double a, const double b) {
     if (b == 0) {
-        warning(1);
+        warning();
         return a;
     }
     return a / b;
 }
 double mod(const double a, const double b) {
+    if (b == 0) warning();
     return a - (int)(a/b)*b;
 }
-void clear_mem(){}
+void clear_mem() {
+    mem_used = false;
+    cout<<mult(mem,0)<<endl;
+}
+void close_app() {
+    exit(0);
+}
+void is_from_range(unsigned int* ptr) {//działa dla znakow i liczb poza zakresem, nie dziala dla "1-"
+    while (true) {
+        if (std::cin >> *ptr) {
+            if(*ptr >= 1 and *ptr <=7) {
+                break;
+            }std::cout << "Wpisz liczbe z przedzialu 1-7\n";
+        } else {
+            std::cout << "Wpisz liczbe z przedzialu 1-7\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            *ptr = 0;
+        }
+    }
+}
 void menu() {
     double a = 0;
-    cout<<" 1) +\n 2) - \n 3) * \n 4) / \n 5) % \n 6) Wyzeruj\n";
-    if(mem_used==1)cin>>mem;
-        unsigned int opt;
+    unsigned int opt;
+    unsigned int* ptr = &opt;
+    if(mem_used==0) {
+        cin>>mem;
+        mem_used = true;
+    }
+    cout<<" 1) +\n 2) - \n 3) * \n 4) / \n 5) % \n 6) Wyzeruj\n 7) Zamknij program\n";
+    //cin>>opt;
+    is_from_range(ptr);
         switch (opt) {
             case 1:
+                cout<<mem<<"+";
                 cin>>a;
-                add(mem, a);
+                mem = add(mem, a);
+                cout<<mem<<endl;
                 break;
             case 2:
+                cout << mem << "-";
                 cin>>a;
-                sub(mem, a);
+                mem = sub(mem, a);
+                cout<<mem<<endl;
                 break;
             case 3:
+                cout<<mem<<"*";
                 cin>>a;
-                mult(mem, a);
+                mem = mult(mem, a);
+                cout<<mem<<endl;
                 break;
             case 4:
+                cout<<mem<<"/";
                 cin>>a;
-                div(mem, a);
+                mem = div(mem, a);
+                cout<<mem<<endl;
                 break;
             case 5:
+                cout<<mem<<"%";
                 cin>>a;
-                mod(mem, a);
+                mem = mod(mem, a);
+                cout<<mem<<endl;
                 break;
-            case 6: clear_mem();
+            case 6:clear_mem();
+                break;
+            case 7:
+                cout<<"Wylaczam kalkulator\n";
+                close_app();
                 break;
         }
 }
 int main(){
-    menu();
+    cout<<"Kalkulator wlaczony, wpisz liczbe\n";
+    while(true)menu();
+
+
     return 0;
 }

af78d0456aac5f3551981d9727c994b78f2858a9
Fr0ggi3 <287084@student.pwr.edu.pl>
2025-03-25 18:30:38 +0100

    converted calc into class
    not working endlessly, need a fix

--- Calculator.cpp
+++ Calculator.cpp
@@ -1,107 +1,106 @@
 #include <iostream>
 #include <limits>
 using namespace std;
-double mem;
-bool mem_used; //sprawdza czy uzyto pamieci, jesli tak to pobieramy tylko jedna liczbe, jesli nie to 2
-//dodatkowo przeliczanie baz -> podac baze, liczbe i na jaki system liczbowy go zmienić
-void warning() {
-        cout<<"Dzielenie przez 0 jest niedozwolone, wyswietle poprzednia wartosc \n"<<endl;
-}
-double add(const double a, const double b) {
-    return a + b;
-}
 
-double sub(const double a, const double b) {
-    return a - b;
-}
-double mult(const double a, const double b) {
-    return a * b;
-}
-double div(const double a, const double b) {
-    if (b == 0) {
-        warning();
-        return a;
+class Calculator {
+private:
+    double mem = 0;
+    //sprawdza czy uzyto pamieci, jesli tak to pobieramy tylko jedna liczbe, jesli nie to 2
+    //dodatkowo przeliczanie baz -> podac baze, liczbe i na jaki system liczbowy go zmienić
+public:
+    bool mem_used = false;
+    void warning() {
+        cout<<"Dzielenie przez 0 jest niedozwolone, wyswietle poprzednia wartosc \n"<<endl;
     }
-    return a / b;
-}
-double mod(const double a, const double b) {
-    if (b == 0) warning();
-    return a - (int)(a/b)*b;
-}
-void clear_mem() {
-    mem_used = false;
-    cout<<mult(mem,0)<<endl;
-}
-void close_app() {
-    exit(0);
-}
-void is_from_range(unsigned int* ptr) {//działa dla znakow i liczb poza zakresem, nie dziala dla "1-"
-    while (true) {
-        if (std::cin >> *ptr) {
-            if(*ptr >= 1 and *ptr <=7) {
-                break;
-            }std::cout << "Wpisz liczbe z przedzialu 1-7\n";
-        } else {
-            std::cout << "Wpisz liczbe z przedzialu 1-7\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-            *ptr = 0;
+    double add(const double a, const double b) {
+        return a + b;
+    }
+
+    double sub(const double a, const double b) {
+        return a - b;
+    }
+    double mult(const double a, const double b) {
+        return a * b;
+    }
+    double div(const double a, const double b) {
+        if (b == 0) {
+            warning();
+            return a;
         }
+        return a / b;
     }
-}
-void menu() {
-    double a = 0;
-    unsigned int opt;
-    unsigned int* ptr = &opt;
-    if(mem_used==0) {
-        cin>>mem;
-        mem_used = true;
+    double mod(const double a, const double b) {
+        if (b == 0) warning();
+        return a - (int)(a/b)*b;
     }
-    cout<<" 1) +\n 2) - \n 3) * \n 4) / \n 5) % \n 6) Wyzeruj\n 7) Zamknij program\n";
-    //cin>>opt;
-    is_from_range(ptr);
+    void clear_mem() {
+        mem_used = false;
+        cout<<mult(mem,0)<<endl;
+    }
+
+    void close_app() {
+        cout<<"Wylaczam kalkulator\n";
+        exit(0);
+    }
+    double get() {
+        return mem;
+    }
+    void set(double number) {
+        mem = number;
+    }
+};
+    void menu(Calculator calc) {
+        double a = 0;
+        unsigned int opt;
+        if(calc.mem_used==0) {
+            cin >> a;
+            calc.set(a);
+            calc.mem_used = true;
+        }
+        cout<<" 1) +\n 2) - \n 3) * \n 4) / \n 5) % \n 6) Wyzeruj\n 7) Zamknij program\n";
+        cin>>opt;
         switch (opt) {
             case 1:
-                cout<<mem<<"+";
-                cin>>a;
-                mem = add(mem, a);
-                cout<<mem<<endl;
-                break;
+                cout<<calc.get()<<"+";
+            cin>>a;
+            calc.set(calc.add(calc.get(), a));
+            cout<<calc.get()<<endl;
+            break;
             case 2:
-                cout << mem << "-";
-                cin>>a;
-                mem = sub(mem, a);
-                cout<<mem<<endl;
-                break;
+                cout << calc.get()<< "-";
+            cin>>a;
+            calc.set(calc.sub(calc.get(), a));
+            cout<<calc.get()<<endl;
+            break;
             case 3:
-                cout<<mem<<"*";
-                cin>>a;
-                mem = mult(mem, a);
-                cout<<mem<<endl;
-                break;
+                cout<<calc.get()<<"*";
+            cin>>a;
+            calc.set(calc.mult(calc.get(), a));
+            cout<<calc.get()<<endl;
+            break;
             case 4:
-                cout<<mem<<"/";
-                cin>>a;
-                mem = div(mem, a);
-                cout<<mem<<endl;
-                break;
+                cout<<calc.get()<<"/";
+            cin>>a;
+            calc.set(calc.div(calc.get(),a));
+            cout<<calc.get()<<endl;
+            break;
             case 5:
-                cout<<mem<<"%";
-                cin>>a;
-                mem = mod(mem, a);
-                cout<<mem<<endl;
-                break;
-            case 6:clear_mem();
-                break;
+                cout<<calc.get()<<"%";
+            cin>>a;
+            calc.set(calc.mod(calc.get(),a));
+            cout<<calc.get()<<endl;
+            break;
+            case 6:calc.clear_mem();
+            break;
             case 7:
-                cout<<"Wylaczam kalkulator\n";
-                close_app();
-                break;
+            calc.close_app();
+            break;
         }
-}
+    }
 int main(){
+        Calculator calc;
     cout<<"Kalkulator wlaczony, wpisz liczbe\n";
-    while(true)menu();
+    while(true)menu(calc);
 
 
     return 0;
